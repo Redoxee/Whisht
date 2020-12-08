e5877d491487087e20995ad6f0ed12867eadd4f6
Anton Roy <antonmakesgames@gmail.com>
2020-12-09 00:38:12 +0100

    Created a mcokup http server.

---
+++ WistGame/WistServer/WebServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace WebServer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add("http://+:80/");
+//            server.Prefixes.Add("http://localhost/");
+
+            server.Start();
+
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                HttpListenerContext context = server.GetContext();
+                HttpListenerResponse response = context.Response;
+
+                string msg = "<html><body><br>HELLO</br></body></html>";
+
+                byte[] buffer = Encoding.UTF8.GetBytes(msg);
+
+                response.ContentLength64 = buffer.Length;
+                Stream st = response.OutputStream;
+                st.Write(buffer, 0, buffer.Length);
+
+                context.Response.Close();
+            }
+
+        }
+    }
+
+}
