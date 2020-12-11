9d442e52ecc40e5470fe112f8c7a6e7d774ebfd1
Anton Roy <antonmakesgames@gmail.com>
2020-12-11 01:10:42 +0100

    Started experimentation with web sockets.

--- WistGame/WebWist/Startup.cs
+++ WistGame/WebWist/Startup.cs
@@ -7,6 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Resources;
+using System.Net.WebSockets;
+using System.Threading;
 
 namespace WebWist
 {
@@ -27,18 +30,49 @@ namespace WebWist
             }
 
             app.UseRouting();
+            app.UseWebSockets();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", this.defaultEndPoint);
+                endpoints.MapGet("/Game", this.gameEndPoint);
             });
         }
 
         private async Task defaultEndPoint(HttpContext context)
         {
-            GameProcess gameProcess = GameProcess.Instance;
-            var gm = gameProcess.GetGameManager();
-            await context.Response.WriteAsync(gm.GetDebugString());
+            ResourceManager resourceManager = new ResourceManager("WebWist.GameResources",typeof(Startup).Assembly);
+            string indexFile = (resourceManager.GetString("GameIndex"));
+            await context.Response.WriteAsync(indexFile);
+        }
+
+        private async Task gameEndPoint(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+                {
+                    
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task Echo(HttpContext context, WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                System.Console.WriteLine("Recieving web socket data.");
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
