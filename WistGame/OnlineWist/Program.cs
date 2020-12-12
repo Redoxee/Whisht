975c5530bc312f757c573f8b11fc8e526206e46f
Anton Roy <antonmakesgames@gmail.com>
2020-12-12 15:58:35 +0100

    Trying a new approach with a simpler web server.

---
+++ WistGame/OnlineWist/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+using System.Resources;
+
+namespace OnlineWist
+{
+    class Program
+    {
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            ResourceManager resourceManager = new ResourceManager("OnlineWist.GameResources", typeof(Program).Assembly);
+            string indexFile = (resourceManager.GetString("GameIndex"));
+            return indexFile;
+        }
+
+        private static void Main(string[] args)
+        {
+            var ws = new WebServer(SendResponse, "http://localhost:8080/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+    }
+}
